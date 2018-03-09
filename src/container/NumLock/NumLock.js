09a894c79cc78aed34d4afc5ecdc3ea1e03fecb6
Saver2003 <saver2003@gmail.com>
2018-03-09 18:31:25 +0600

    added keycodes

--- src/container/NumLock/NumLock.js
+++ src/container/NumLock/NumLock.js
@@ -4,10 +4,26 @@ import './NumLock.css';
 import {connect} from 'react-redux';
 
 class NumLock extends Component {
+  componentDidMount() {
+    document.body.onkeydown = (e) => {
+      if (e.keyCode >= 96 && e.keyCode <= 105) {
+        this.props.addNum(e.key);
+      }
+      
+      if (e.keyCode === 13) {
+        console.log(e.key);
+        this.props.validNum();
+      }
+      
+      if (e.keyCode === 8) {
+        this.props.deleteNum();
+      }
+    }
+  }
   render() {
     let buttons = [];
     for (let i = 0; i < 10; i++) {
-      buttons.push(<button onClick={() => this.props.addNum(i)}>{i}</button>);
+      buttons.push(<button onClick={() => this.props.addNum(i)} key={i}>{i}</button>);
     }
     return (
       <div className="NumLock">
